ff2eeae575eacf49ff0ba01afc19bb77df951972
salah-alsilevanai <salah.alsilevanai@gmail.com>
2026-06-08 21:26:19 +0300

    add timer for waiting

--- Content.js
+++ Content.js
@@ -1,6 +1,7 @@
 
 while (!document.querySelector("video")) {
     console.log("Waiting for video element to load...");
+    setTimeout(() => {}, 1000); // Wait for 1 second before checking again
 }
 const video = document.querySelector("video");
 
