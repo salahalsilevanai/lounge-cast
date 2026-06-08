4cf1895f3d38f9368b90df2b1f15af0617b936a5
salah-alsilevanai <salah.alsilevanai@gmail.com>
2026-06-08 21:51:10 +0300

    add p and s funtionality

--- Content.js
+++ Content.js
@@ -1,16 +1,51 @@
 
-while (!document.querySelector("video")) {
-    console.log("Waiting for video element to load...");
-    setTimeout(() => {}, 1000); // Wait for 1 second before checking again
-}
-const video = document.querySelector("video");
+function check_video() {
 
+    if (!document.querySelector("video")) {
+        setTimeout(check_video, 500); // Wait for 1 second before checking again
+    } else {
+        console.log("Video element found!");
+    }
+}
+check_video();
+
+
+
+// current time = document.querySelector("video").currentTime
+// duration = document.querySelector("video").duration
+// You can use these properties to get the current time and duration of the video. For example:
+// other properiteis
+// video.play() - to play the video
+// video.pause() - to pause the video
+// video.currentTime = 10 - to set the current time to 10 seconds
+// You can also add event listeners to the video element to track when the video is played, paused, or ended. For example:
+// video.addEventListener("play", function() {
+//     console.log("Video is playing");
+// });
+// video.addEventListener("pause", function() {
+//     console.log("Video is paused");
+// });
+// video.addEventListener("ended", function() {
+//     console.log("Video has ended");
+// });
 
 
 function get_current_time(){
-    console.log(video.currentTime)
+    console.log(document.querySelector("video").currentTime)
+    console.log(document.querySelector("video").duration)
 }
+// if key is press "p" then pause the video
+document.addEventListener("keydown", function(event) {
+    if (event.key === "p") {
+        document.querySelector("video").pause();
+    }
+});
 
-
+// if key is press "s" then play the video
+document.addEventListener("keydown", function(event) {
+    if (event.key === "s") {
+        document.querySelector("video").play();
+    }
+});
 
 setInterval(get_current_time, 1000)
