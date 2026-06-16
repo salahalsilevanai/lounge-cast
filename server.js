cb54c03500dd8c59f72b5c864640e60e8fca087c
salah-alsilevanai <salah.alsilevanai@gmail.com>
2026-06-16 09:11:37 +0300

    add server console logs to properly debug errors

--- server.js
+++ server.js
@@ -27,23 +27,27 @@ io.on("connection", (socket) => {
         roomState.currentTime = packet.time;
         roomState.lastUpdated = Date.now();
         console.log("PLAY");
+        console.log(roomState);
         break;
       case "VIDEO_PAUSE":
         roomState.isPlaying = false;
         roomState.currentTime = packet.time;
         roomState.lastUpdated = Date.now();
         console.log("PAUSE");
+        console.log(roomState);
         break;
       case "VIDEO_SEEK":
         roomState.currentTime = packet.time;
         roomState.lastUpdated = Date.now();
         console.log("SEEK");
+        console.log(roomState);
         break;
       case "CHAT_MSG":
         console.log(packet.text);
+        console.log(roomState);
         break;
     }
-    // Broadcast the updated state to everyone else
+    //Broadcast the updated state to everyone else
     socket.broadcast.emit("watch_party_event", packet);
     //socket.emit("watch_party_event", packet);
   });
